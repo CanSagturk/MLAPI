3bb8cf9d367be31aca1f99ad6197e20bef7b0223
Can Sagturk <C.Sagturk@student.tudelft.nl>
2023-02-22 12:17:06 +0100

    Initial commit

---
+++ src/main/java/com/modulonion/mlapi/controller/LeaderboardsController.java
@@ -0,0 +1,11 @@
+package com.modulonion.mlapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("leaderboards")
+public class LeaderboardsController {
+
+
+}
