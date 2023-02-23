295cb1d5021b4b055fa122eec42d402be1bfc964
Can Sagturk <C.Sagturk@student.tudelft.nl>
2023-02-23 11:51:43 +0100

    Initial controller and application properties

--- src/main/java/com/modulonion/mlapi/controller/LeaderboardsController.java
+++ src/main/java/com/modulonion/mlapi/controller/LeaderboardsController.java
@@ -1,11 +1,14 @@
 package com.modulonion.mlapi.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("leaderboards")
 public class LeaderboardsController {
-
-
+    @GetMapping("/")
+    public void GetLeaderboards() {
+        System.out.println("Hello, World!");
+    }
 }
